be0f06b3cc0549634d9c03b2f10732da9d269628
OlgaU <ovyurova@yandex.ru>
2024-01-27 11:17:44 +0300

    1

---
+++ MLP_pytorch_main_Y.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+import pandas as pd
+import numpy as np
+from neural import MLPptorch
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+
+# функция обучения
+def train(x, y, num_iter):
+    for i in range(0,num_iter):
+        pred = net.forward(x)
+        loss = lossFn(pred, y)
+        loss.backward()
+        optimizer.step()
+        if i%(num_iter/10)==0:
+           print('Ошибка на ' + str(i) + ' итерации: ', loss.item())
+    return loss.item()
+
+
+df = pd.read_excel('res1mod1.xls')
+df.to_csv('res1mod2.csv')
+
+df = pd.read_csv('res1mod2.csv')
+df = df.iloc[np.random.permutation(len(df))]
+
+X = df.iloc[:, 3:-1].values
+y = df.iloc[:, -1].values
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.3) #разбиваем выборку на обучающую и тестовую
+
+Y = np.zeros((y.shape[0], np.unique(y).shape[0]))
+
+
+inputSize = X.shape[1] # количество входных сигналов равно количеству признаков задачи 
+hiddenSizes = 150 # задаем число нейронов скрытого слоя 
+outputSize = Y.shape[1] if len(Y.shape) else 1 # количество выходных сигналов равно количеству классов задачи
+
+
+net = MLPptorch(inputSize,hiddenSizes,outputSize)
+lossFn = nn.MSELoss()
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.05)
+
+loss_ = train(torch.from_numpy(X.astype(np.float32)), 
+              torch.from_numpy(Y.astype(np.float32)), 5000)
+
+
+pred = net.forward(torch.from_numpy(X.astype(np.float32))).detach().numpy()
+err = sum(abs((pred>0.5)-Y))
+print(err)   
+
+pred = net.forward(torch.from_numpy(X_test.astype(np.float32))).detach().numpy()
+err = sum(abs((pred>0.5)-Y_test))
+print(err)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+# настройка параметров модели 
+model = LogisticRegression()
+y_scor = model.fit(X_train, Y_train)
+y_score = y_scor.predict(X_test)
+
+# оценка точности модели
+print(metrics.classification_report(Y_test, y_score))
